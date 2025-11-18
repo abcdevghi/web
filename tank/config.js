326520629deaf2d519b1b8e162ef69ef6219c297
trueuser <trueuser@duck.com>
2025-11-18 17:26:44 +0700

    a

--- tank/config.js
+++ tank/config.js
@@ -3,7 +3,7 @@
 export const TERR_WIDTH = 1000;
 export const TERR_HEIGHT = 800;
 export const TANK_W = 18;
-export const TANK_H = 6;
+export const TANK_H = 7;
 export const GRAVITY = 0.5;
 export const MAX_SLOPE = 3;
 export const CELEBRATION_TIME = 10000;
